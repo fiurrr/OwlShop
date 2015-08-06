aeaad637a2e2f46252f49baf42a442eb13aa12a0
fiurrr <jestem.wyjebisty@gmail.com>
2015-08-06 14:44:25 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ productApp.config(['$routeProvider',
         when('/', {
             templateUrl: '/shop/public/html/main-page.html',
             controller: 'productListCtrl'
+            fajna konwencja jest main.product.list i np main.product.add wtedy, html katalog moozna zmienic nazwe na views albo templates ale to detal
         }).
         when('/product/:productId', {
             templateUrl: '/shop/public/html/single-product.html',
